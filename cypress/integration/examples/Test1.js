881796c6d7de479b57ec6d724de433dc96808c6b
amitech17 <amitmech17@gmail.com>
2021-02-13 13:43:41 +0530

    second

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -3,12 +3,15 @@
 describe('Test Suite', function() 
 {
 
-it('Test case',function() {
+it('Test case - Dropdown',function() {
 
 //Dropdown Test
 cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
 cy.get('select').select('option3').should('have.value','option3')
 
+}  )
+
+it('Test Case - suggestion',function(){
 //suggestion Test
 cy.get('#autocomplete').type('us')
 cy.get('.ui-menu-item div').each(($element, index, $list) => {
@@ -20,12 +23,13 @@ cy.get('.ui-menu-item div').each(($element, index, $list) => {
 //checking value of selection
 cy.get('#autocomplete').should('have.value','United States (USA)')
 
+})
+
+it('Test Case - Hover', function(){
 //Mouse Hover Test
 cy.get('.mouse-hover-content').invoke('show')
 cy.contains('Top').click()
 cy.url().should('include','#top')
+})
 
-
-}  )
-
-}  )
+})
